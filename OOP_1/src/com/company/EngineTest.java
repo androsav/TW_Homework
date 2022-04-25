eceb441a7b85b1e4dec1071c25aea5db0986ddfd
androsav <purikdanila@gmail.com>
2022-04-25 22:28:02 +1000

    -

--- OOP_1/src/com/company/EngineTest.java
+++ OOP_1/src/com/company/EngineTest.java
@@ -1,16 +1,24 @@
 package com.company;
+import org.junit.jupiter.api.Test;
 
-import org.testng.annotations.Test;
 
 class EngineTest {
-    Engine testEngine = new Engine("400 hp","8 liters", 12000 );
+
     @Test
-    void bigGas() {
+     void bigGas() {
+        // Arrange
+        Engine testEngine = new Engine("400 hp","8 liters", 12000 );
+        // Act
         testEngine.Gas(120000);
+        // Assert
     }
 
     @Test
     void zeroGas() {
+        // Arrange
+        Engine testEngine = new Engine("400 hp","8 liters", 12000 );
+        // Act
         testEngine.Gas(0);
+        // Assert
     }
 }
--- OOP_1/src/com/company/Main.java
+++ OOP_1/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+
         Engine trainEngine = new Engine("400 hp","40 liters", 1600 );
 	    Train train = new Train("Electric locomotive", "Passenger", trainEngine,"ER1");
         Engine carEngine = new Engine("280 hp","2,4 liters", 7000 );
@@ -14,6 +16,7 @@ public class Main {
 
         car.MoveForward();
         car.StepOnGasPedal();
+
         car.StepOnGasPedal();
         car.StepOnGasPedal();
         car.StepOnGasPedal();
